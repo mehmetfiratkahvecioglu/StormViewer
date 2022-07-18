d163c3aaab96a5eeb3889859192a0fcee5c09431
Fırat <mehmetfirat0707@hotmail.com>
2022-07-18 11:20:50 +0300

    Project 3 add UIActivityController to DetailViewController and done Project 3 Challenges 1-2

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 class DetailViewController: UIViewController {
     
     @IBOutlet var imageView: UIImageView!
+    
     var selectedImage: String?
     var totalNumberofPics: Int?
     var indexPathOfSelectedImage: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
         navigationItem.largeTitleDisplayMode = .never
+        
+        //for project 3
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
@@ -49,6 +52,21 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
 
+    // for project3
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        //project 3 challenge 1
+        var shareable: [Any] = [image]
+               if let imageText = selectedImage {
+                   shareable.append(imageText)
+               }
+        
+        let vc = UIActivityViewController(activityItems: shareable, applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     
-
+    }
 }
--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UITableViewController {
         title = "Storm Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        // project 3 challenge 2
+                navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -51,6 +54,16 @@ class ViewController: UITableViewController {
         }
     }
     
+    @objc func shareTapped() {
+            var items: [Any] = ["This app is great, you should try it!"]
+            if let url = URL(string: "https://www.hackingwithswift.com/100/16") {
+                items.append(url)
+            }
+            
+            let vc = UIActivityViewController(activityItems: items, applicationActivities: [])
+            vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+            present(vc, animated: true)
+        }
     
     
 }
